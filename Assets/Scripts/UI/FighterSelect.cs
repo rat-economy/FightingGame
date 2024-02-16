d3ef566566d5a49ecbaa389becb34f2d8b56ec86
Roosevelt Sinker <66215548+rsinker@users.noreply.github.com>
2024-02-15 21:16:07 -0800

    Finished lock in and 2nd player

--- Assets/Scripts/UI/FighterSelect.cs
+++ Assets/Scripts/UI/FighterSelect.cs
@@ -4,24 +4,55 @@ using UnityEngine;
 
 public class FighterSelect : MonoBehaviour
 {
+    [SerializeField] private GameObject selectionFrame;
+    [SerializeField] private GameObject selectionButton;
+
+
+    bool isTwoPlayer = false; //To test 2 player: change to true
     bool currPlayer = false;
-    public void FighterSelectButton(int nameIndex)
+    int currentlySelectedFighter;
+
+    Vector2[] frameLocationPerCharacter = new Vector2[] {new Vector2(-375, 125), new Vector2(-125, 125), new Vector2(125, 125), new Vector2(375, 125), new Vector2(-375, -125), new Vector2(-125, -125), new Vector2(125, -125), new Vector2(375, -125)};
+
+    public void ClickFighter(int nameIndex)
     {
-        CharacterName name = (CharacterName)nameIndex;
+        currentlySelectedFighter = nameIndex;
+
+        selectionFrame.SetActive(true);
+        selectionButton.SetActive(true);
+
+        selectionFrame.GetComponent<RectTransform>().anchoredPosition = frameLocationPerCharacter[nameIndex];
+    }
+
+    public void FighterSelectButton()
+    {
+        CharacterName name = (CharacterName)currentlySelectedFighter;
         SelectCharacter(name, currPlayer);
 
-        GameManager.Instance.StartInitializeRound(false); //Passes in false for now, so always single player. Can change later.
+        if (currPlayer == false)
+        {
+            currPlayer = true;
+        }
+        else
+        {
+            GameManager.Instance.StartInitializeRound(isTwoPlayer); //Passes in false for now, so always single player. Can change later.
+        }
     }
     //Move to playerselect script
     private void SelectCharacter(CharacterName name, bool player)
     {
+        selectionFrame.SetActive(false);
+        selectionButton.SetActive(false);
+
         Character c = FindCharacter(name);
         if (player == false)
         {
+            Debug.Log("Assigning Player 1: " + name);
             GameManager.p1_selectedCharacter = c;
         }
         else
         {
+            Debug.Log("Assigning Player 2: " + name);
             GameManager.p2_selectedCharacter = c;
         }
     }
